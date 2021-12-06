92e917086039d23fc13f11341e6cfde012f9d8b8
_AG <gennariarmando@outlook.com>
2021-12-06 09:58:58 +0100

    Improve small help text size

--- source/HudNew.cpp
+++ source/HudNew.cpp
@@ -1447,7 +1447,7 @@ void CHudNew::PrintSmallHelpText(int alpha) {
         r.left = HUD_X(96.0f);
         r.top = HUD_BOTTOM(324.0f);
         r.right = r.left + SCREEN_COORD(270.0f);
-        r.bottom = r.top + SCREEN_COORD(64.0f);
+        r.bottom = r.top + SCREEN_COORD(69.0f);
 
         DrawSimpleRectGrad(r, c);
 
@@ -1463,20 +1463,20 @@ void CHudNew::PrintSmallHelpText(int alpha) {
         c = GET_SETTING(HUD_HELP_BOX_SMALL_TEXT).col;
         CFontNew::SetColor(CRGBA(c.r, c.g, c.b, clamp(alpha, 0, c.a)));
         CFontNew::SetScale(SCREEN_MULTIPLIER(0.6f), SCREEN_MULTIPLIER(1.2f));
-        CFontNew::PrintString(HUD_X(96.0f + 50.0f), HUD_BOTTOM(324.0f), string);
-        CFontNew::PrintString(HUD_X(96.0f + 50.0f), HUD_BOTTOM(304.0f), percentage);
+        CFontNew::PrintString(HUD_X(96.0f + 62.0f), HUD_BOTTOM(326.0f), string);
+        CFontNew::PrintString(HUD_X(96.0f + 62.0f), HUD_BOTTOM(306.0f), percentage);
 
         CheckPlayerPortrait(CWorld::PlayerInFocus);
-        r.left = HUD_X(96.0f + 4.0f);
-        r.top = HUD_BOTTOM(324.0f - 4.0f);
-        r.right = r.left + SCREEN_COORD(38.0f);
-        r.bottom = r.top + SCREEN_COORD(38.0f);
+        r.left = HUD_X(96.0f);
+        r.top = HUD_BOTTOM(324.0f);
+        r.right = r.left + SCREEN_COORD(54.0f);
+        r.bottom = r.top + SCREEN_COORD(54.0f);
         DrawSimpleRect(r, HudColourNew.GetRGB(HUD_COLOUR_BLACK, 200));
         PlayerPortrait[CWorld::PlayerInFocus][1]->Draw(r, CRGBA(255, 255, 255, 255));
         unsigned int savedFilter;
         RwRenderStateGet(rwRENDERSTATETEXTUREFILTER, (void*)&savedFilter);
         RwRenderStateSet(rwRENDERSTATETEXTUREFILTER, (void*)rwFILTERNEAREST);
-        DrawProgressBarWithSprite(StatsSprites[PLRSTAT_PROGRESS_BAR], HUD_X(96.0f + 4.0f), HUD_BOTTOM(300.0f - 23.0f), SCREEN_COORD(262.0f), SCREEN_COORD(13.0f), progress / 1000, HudColourNew.GetRGB(MenuNew.Settings.uiMainColor, 255));
+        DrawProgressBarWithSprite(StatsSprites[PLRSTAT_PROGRESS_BAR], HUD_X(96.0f), r.bottom, SCREEN_COORD(270.0f), SCREEN_COORD(15.0f), progress / 1000, HudColourNew.GetRGB(MenuNew.Settings.uiMainColor, 255));
         RwRenderStateSet(rwRENDERSTATETEXTUREFILTER, (void*)savedFilter);
     }
     else if (ShowTripSkipMessage) {
